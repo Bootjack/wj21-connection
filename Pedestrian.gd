e76785fbb9d7814f19c01cc2217c428b6816c319
Jason Hinebaugh <jason.hinebaugh@customink.com>
2020-05-16 20:39:49 -0400

    About to integrate Infectors into Pedestrians

--- Pedestrian.gd
+++ Pedestrian.gd
@@ -3,7 +3,6 @@ extends Person
 
 export var direction:Vector2 = Vector2.ZERO
 export var distance:float = 0.0
-export var infectiousness:float = 2.0
 export var wait_time:float = 0.0
 
 var reference_point:Vector2
@@ -18,17 +17,10 @@ func _ready():
 	add_child(wait_timer)
 	wait_timer.connect("timeout", self, "_on_wait_timeout")
 
-	$Visualization/InfectionZone.connect("body_entered", self, "infect")
-	$Visualization/InfectionZone.connect("body_exited", self, "disinfect")
-
 func _on_wait_timeout():
 	is_waiting = false
 	reference_point = global_position
 	turn_around()
-
-func disinfect(body:Node2D):
-	if (body.is_class("Player")):
-		body.emit_signal("disinfected", infectiousness)
 
 func get_class():
 	return "Pedestrian"
@@ -43,10 +35,6 @@ func handle_movement():
 		wait()
 	
 	parent.resume()
-
-func infect(body:Node2D):
-	if (body.is_class("Player")):
-		body.emit_signal("infected", infectiousness)
 	
 func is_class(name:String):
 	return name == "Pedestrian" or .is_class(name)
